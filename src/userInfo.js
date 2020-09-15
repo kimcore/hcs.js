568e51090cb98449347e03f015a9dae2f6864c18
kimcore <kimcoreovo@gmail.com>
2020-09-15 21:04:10 +0900

    1.0.4

--- src/userInfo.js
+++ src/userInfo.js
@@ -9,7 +9,7 @@ module.exports = async (endpoint, token) => {
             orgCode: user['orgCode'],
             userPNo: user['userPNo']
         }
-        const userinfo = await request('/userrefresh', 'POST', data, endpoint, {'Authorization': token})
+        const userinfo = (await request('/userrefresh', 'POST', data, endpoint, {'Authorization': token}))['UserInfo']
         list.push({
             registerRequired: userinfo['registerDtm'] === undefined,
             registeredAt: userinfo['registerDtm'],
