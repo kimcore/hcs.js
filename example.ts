0a86aba3e295ee6c8f097666cdcf5925dcf25271
kimcore <kimcoreovo@gmail.com>
2022-02-26 22:10:38 +0900

    2.0.6: Update questions

--- example.ts
+++ example.ts
@@ -3,6 +3,7 @@ import * as hcs from "./src"
 
 // @ts-ignore
 import Readline from 'readline'
+import {CovidQuickTestResult} from "./src";
 
 const readline = Readline.createInterface({
     input: process.stdin,
@@ -78,18 +79,31 @@ const example = async () => {
     }
 
     const survey = {
-        // 1. 학생 본인이 코로나19가 의심되는 아래의 임상증상*이 있나요?
-        // * (주요 임상증상) 발열(37.5℃ 이상), 기침, 호흡곤란, 오한, 근육통, 두통, 인후통, 후각·미각소실
+        /**
+         * 1. 학생 본인이 코로나19 감염에 의심되는 아래의 임상증상*이 있나요?
+         * (주요 임상증상) 발열(37.5℃), 기침, 호흡곤란, 오한, 근육통, 두통, 인후통, 후각·미각소실
+         */
         Q1: false,
 
-        // 2. 학생 본인 또는 동거인이 코로나19 진단검사를 받고 그 결과를 기다리고 있나요?
-        Q2: false,
+        /**
+         * 2. 학생은 오늘 신속항원검사(자가진단)를 실시했나요?
+         */
+        Q2: CovidQuickTestResult.NONE,
 
-        // 3. 학생 본인 또는 동거인이 방역당국에 의해 현재 자가격리가 이루어지고 있나요?
+        /**
+         * 3.학생 본인 또는 동거인이 PCR 검사를 받고 그 결과를 기다리고 있나요?
+         */
         Q3: false,
 
-        // 4. 학생의 동거인 중 확진자가 있나요?
-        Q4: false
+        /**
+         * 4. 학생 본인이 보건소로부터 밀접접촉자로 통보받아 현재 자가격리 중인가요?
+         */
+        Q4: false,
+
+        /**
+         * 5. 학생의 동거인 중 재택치료자가 있어 공동격리인으로 지정되어 현재 자가격리 중인가요?
+         */
+        Q5: false
     }
     const result = await hcs.registerSurvey(school.endpoint, token, survey)
     console.log(`${result.registeredAt} ${login.name}님 자가진단을 완료하셨습니다.`)
--- src/registerSurvey.ts
+++ src/registerSurvey.ts
@@ -1,33 +1,47 @@
 import request from "./request"
 import {userInfo} from "./userInfo"
 
+export enum CovidQuickTestResult {
+    /** 검사하지 않음 */
+    NONE,
+    /** 음성 */
+    NEGATIVE,
+    /** 양성 */
+    POSITIVE
+}
+
 /** 설문 내용 */
 export interface SurveyData {
-    /*
-     * 1. 학생 본인이 코로나19가 의심되는 아래의 임상증상*이 있나요?
-     * (주요 임상증상) 발열(37.5℃ 이상), 기침, 호흡곤란, 오한, 근육통, 두통, 인후통, 후각·미각소실
+    /**
+     * 1. 학생 본인이 코로나19 감염에 의심되는 아래의 임상증상*이 있나요?
+     * (주요 임상증상) 발열(37.5℃), 기침, 호흡곤란, 오한, 근육통, 두통, 인후통, 후각·미각소실
      */
     Q1: boolean
 
-    /*
-     * 2. 학생 본인 또는 동거인이 코로나19 진단검사를 받고 그 결과를 기다리고 있나요?
+    /**
+     * 2. 학생은 오늘 신속항원검사(자가진단)를 실시했나요?
      */
-    Q2: boolean
+    Q2: CovidQuickTestResult
 
-    /*
-     * 3.학생 본인 또는 동거인이 방역당국에 의해 현재 자가격리가 이루어지고 있나요?
+    /**
+     * 3.학생 본인 또는 동거인이 PCR 검사를 받고 그 결과를 기다리고 있나요?
      */
     Q3: boolean
 
-    /*
-     * 4. 학생의 동거인 중 확진자가 있나요?
+    /**
+     * 4. 학생 본인이 보건소로부터 밀접접촉자로 통보받아 현재 자가격리 중인가요?
      */
     Q4: boolean
+
+    /**
+     * 5. 학생의 동거인 중 재택치료자가 있어 공동격리인으로 지정되어 현재 자가격리 중인가요?
+     */
+    Q5: boolean
 }
 
 /** 설문 결과 */
 export interface SurveyResult {
-    /**  */
+    /** 설문 시각 */
     registeredAt: string
 }
 
@@ -40,23 +54,24 @@ export interface SurveyResult {
  */
 export async function registerSurvey(endpoint: string, token: string, survey: SurveyData = {
     Q1: false,
-    Q2: false,
+    Q2: CovidQuickTestResult.NONE,
     Q3: false,
-    Q4: false
+    Q4: false,
+    Q5: false
 }): Promise<SurveyResult> {
     const user = await userInfo(endpoint, token)
     const data = {
         deviceUuid: '',
-        rspns00: (!survey.Q1 && !survey.Q2 && !survey.Q3 && !survey.Q4) ? 'Y' : 'N',
+        rspns00: (!survey.Q1 && survey.Q2 !== CovidQuickTestResult.NONE && !survey.Q3 && !survey.Q4 && !survey.Q5) ? 'Y' : 'N',
         rspns01: survey.Q1 ? '2' : '1',
-        rspns02: survey.Q2 ? '0' : '1',
-        rspns03: null,
+        rspns02: survey.Q3 ? '0' : '1',
+        rspns03: survey.Q2 == CovidQuickTestResult.NONE ? '1' : null,
         rspns04: null,
         rspns05: null,
         rspns06: null,
-        rspns07: null,
-        rspns08: survey.Q4 ? '1' : '0',
-        rspns09: survey.Q3 ? '1' : '0',
+        rspns07: survey.Q2 != CovidQuickTestResult.NONE ? (survey.Q2 == CovidQuickTestResult.NEGATIVE ? '0' : '1') : null,
+        rspns08: survey.Q5 ? '1' : '0',
+        rspns09: survey.Q4 ? '1' : '0',
         rspns10: null,
         rspns11: null,
         rspns12: null,
