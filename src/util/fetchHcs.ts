65f0c160e6cdda89cca850dda10453555d4a09fa
kimcore <kimcoreovo@gmail.com>
2022-10-13 03:42:34 +0000

    they patched it, lol

--- src/util/fetchHcs.ts
+++ src/util/fetchHcs.ts
@@ -9,7 +9,7 @@ export const defaultAgent = new Agent({
 })
 
 const defaultHeaders = {
-    "Accept": "application/json, text/plain, */*", "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-GB,en;q=0.9,ko-KR;q=0.8,ko;q=0.7,ja-JP;q=0.6,ja;q=0.5,zh-TW;q=0.4,zh;q=0.3,en-US;q=0.2", "Cache-Control": "no-cache", "Connection": "keep-alive", "Pragma": "no-cache", "Referer": "https://hcs.eduro.go.kr/", "X-Forwarded-For": "hcs.eduro.go.kr", "Sec-Fetch-Dest": "empty", "Sec-Fetch-Mode": "cors", "Sec-Fetch-Site": "same-site", "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1", "X-Requested-With": "XMLHttpRequest",
+    "Accept": "application/json, text/plain, */*", "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-GB,en;q=0.9,ko-KR;q=0.8,ko;q=0.7,ja-JP;q=0.6,ja;q=0.5,zh-TW;q=0.4,zh;q=0.3,en-US;q=0.2", "Cache-Control": "no-cache", "Connection": "keep-alive", "Pragma": "no-cache", "Referer": "https://hcs.eduro.go.kr/", "Sec-Fetch-Dest": "empty", "Sec-Fetch-Mode": "cors", "Sec-Fetch-Site": "same-site", "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1", "X-Requested-With": "XMLHttpRequest",
 }
 
 export default async function fetchHcs(path = "/", method = "GET", data = {}, endpoint = "hcs.eduro.go.kr", token?: string): Promise<any> {
